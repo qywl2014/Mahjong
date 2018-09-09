6047a0c0f1109fd284f0918088bbcd5e1f36af9d
wulang <1359208000@qq.com>
2018-09-09 18:30:59 +0800

    加载麻将图片ok

--- js/Util.js
+++ js/Util.js
@@ -32,7 +32,7 @@ var U = {
     drawImage:function(img,sx,sy,swidth,sheight,x,y,width,height){
         let o=new Object();
         o.context=this.context;
-        o.context.drawImage(img,sx,sy,swidth,sheight,x,y,width,heigh);
+        o.context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
         o.img=img;
         o.sx=sx;
         o.sy=sy;
@@ -42,6 +42,22 @@ var U = {
         o.y=y;
         o.width=width;
         o.height=height;
+        o.clear=function(){
+            o.context.clearRect(o.x-1, o.y-1, o.width+2, o.height+2);
+        }
+        o.up=function(){
+            o.isUp=true;
+            o.clear();
+            o.y=o.y-20;
+            o.context.drawImage(img,sx,sy,swidth,sheight,x,o.y,width,height);
+        }
+        o.down=function(){
+            o.isUp=false;
+            o.clear();
+            o.y=o.y+20;
+            o.context.drawImage(img,sx,sy,swidth,sheight,x,o.y,width,height);
+        }
+        return o;
     }
     
 }
